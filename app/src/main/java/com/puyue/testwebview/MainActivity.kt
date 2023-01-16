a63249035a6d4a5301a373c2b69958d1a2bdc5b0
yue_pu <yue_pu@en-japan.com>
2023-01-16 20:00:15 +0900

    打开本地html文件 注意assert文件夹位置

--- app/src/main/java/com/puyue/testwebview/MainActivity.kt
+++ app/src/main/java/com/puyue/testwebview/MainActivity.kt
@@ -37,12 +37,10 @@ class MainActivity : AppCompatActivity() {
                 handler.proceed() // Ignore SSL certificate errors  //本地服务器无法签名 需要在这里忽略问题
             }
         }
-        webView.loadUrl("https://192.168.10.3:8080/web2.html")//macbook的ip
-
         //加载网页链接
-//        webView.loadUrl("https://192.168.10.3:8080/web2.html")//只能加载https URL http不行
+//        webView.loadUrl("https://192.168.10.3:8080/web2.html")//macbook的ip
 //加载本地assets目录下的网页
-//        webView.loadUrl("file:///web99.html");
+        webView.loadUrl("file:///android_asset/index.html")
 //加载手机本地的html页面
 //        webView.loadUrl("content://com.android.htmlfileprovider/sdcard/keithxiaoy.html");
 //加载 HTML 页面的一小段内容。参数1：需要截取展示的内容、参数2：展示内容的类型、参数3：字节码
